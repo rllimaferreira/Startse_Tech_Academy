eed7850e13d2314940721d111224ea0ec4ff5f1a
Rodrigo L. de Lima Ferreira <rodrigollferreira21@gmail.com>
2022-07-16 18:16:35 -0300

    Upload de atividade e material da aula 17

---
+++ mod10_nodeJS/aula17_iniciando_servidor_com_express/api-rest/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+// Criação de servidor utilizando o framework Express
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+app.get('/', (request, response) => {
+    response.send('<h1>Trabalhando com servidor express.</h2>')
+})
